81a5f7d077c864f8fbafae481692aa6d25014334
SiDDyy007 <siddhantp0@gmail.com>
2023-05-20 12:28:51 -0700

    Working Code

--- main.py
+++ main.py
@@ -1,78 +1,68 @@
 import numpy as np
 
-def dummy_function(data, current_set_of_features, k):
+def dummy_function(data, current_set_of_features):
     # This function returns a random value between 0 and 1.
     return np.random.random()
 
 def feature_search_demo(num_features):
     # Define a dummy dataset with num_features and 10 instances
     data = np.random.rand(10, num_features + 1)
-    
-    current_set_of_features = []  # Initialize empty set
-    # Welcome message
-    print("Welcome to Siddhant's Feature Selection Algorithm.")
-    print(f"Please enter total number of features: {data.shape[1] - 1}")
+
+    # Initialize feature set for both forward selection and backward elimination
+    forward_features = []  
+    backward_features = list(range(num_features)) 
+
+    print(f"Welcome to Siddhant's Feature Selection Algorithm.")
+    print(f"Total number of features: {num_features}")
     print("Type the number of the algorithm you want to run.")
-    print("1. Forward Selection\n2. Backward Elimination\n")
+    print("1. Forward Selection\n2. Backward Elimination")
     choice = int(input())
+
     if choice == 1:
-        forward_selection(data, current_set_of_features)
+        print("Running Forward Selection")
+        features = forward_features
+        print(f"Using no features and “random” evaluation, I get an accuracy of {dummy_function(data, []) * 100}%")
     else:
-        backward_elimination(data, current_set_of_features)
-    
-def forward_selection(data, current_set_of_features):
-    # Using no features and “random” evaluation
-    accuracy = dummy_function(data, current_set_of_features, 0)
-    print(f"Using no features and “random” evaluation, I get an accuracy of {accuracy * 100}%")
-    print("Beginning search.")
+        print("Running Backward Elimination")
+        features = backward_features
+        print(f"Using all features and “random” evaluation, I get an accuracy of {dummy_function(data, backward_features) * 100}%")
 
-    # best_so_far_accuracy = 0
+    print("Beginning search.")
+    search_algorithm(data, features, choice)
+
+def search_algorithm(data, features, choice):
+    accuracy_decreased = False
+    res = features[:]
+    best_so_far_accuracy = 0
     for i in range(data.shape[1] - 1):
+        if accuracy_decreased:
+            break
         print(f'\n On the {i + 1}th level of the search tree')
-        feature_to_add_at_this_level = None
-        best_so_far_accuracy = 0
+        feature_to_change_at_this_level = None
+        
+        features = res[:]
         for k in range(data.shape[1] - 1):
-            if k not in current_set_of_features:
-                print(f'Considering adding the {k + 1} feature')
-                current_set_of_features.append(k)
-                accuracy = dummy_function(data, current_set_of_features, k)
-                print(f'Using feature(s) {current_set_of_features} accuracy is {accuracy * 100}%')
+            if (choice == 1 and k+1 not in features) or (choice == 2 and k+1 in features):
+                print(f'Considering {"adding" if choice == 1 else "removing"} the {k + 1} feature')
+                features.append(k+1) if choice == 1 else features.remove(k+1)
+                accuracy = dummy_function(data, features)
+                print(f'Using feature(s) {features} accuracy is {accuracy * 100}%')
+
                 if accuracy > best_so_far_accuracy:
                     best_so_far_accuracy = accuracy
-                    feature_to_add_at_this_level = k
-                else:
-                    current_set_of_features.remove(k)
-        if feature_to_add_at_this_level is not None:
-            print(f'Feature set {current_set_of_features} was best, accuracy is {best_so_far_accuracy * 100}%')
+                    feature_to_change_at_this_level = k+1
+                # else:
+                features.remove(k+1) if choice == 1 else features.append(k+1)
 
-    print(f'\nFinished search!! The best feature subset is {current_set_of_features}, which has an accuracy of {best_so_far_accuracy * 100}%')
+        if feature_to_change_at_this_level is not None:
+            res.append(feature_to_change_at_this_level) if choice == 1 else res.remove(feature_to_change_at_this_level)
+            print(f'Feature set {res} was best, accuracy is {best_so_far_accuracy * 100}%')
+        else:
+            print("Accuracy has started to decrease... Hence ending the search ")
+            accuracy_decreased = True
+            break
 
-def backward_elimination(data, current_set_of_features):
-    # Using all features and “random” evaluation
-    accuracy = dummy_function(data, current_set_of_features, 0)
-    print(f"Using all features and “random” evaluation, I get an accuracy of {accuracy * 100}%")
-    print("Beginning search.")
-
-    # Iteratively eliminate features
-    for i in range(data.shape[1] - 1):
-        print(f'\n On the {i + 1}th level of the search tree')
-        feature_to_remove_at_this_level = None
-        best_so_far_accuracy = 0
-        for k in range(data.shape[1] - 1):
-            if k in current_set_of_features:
-                print(f'Considering removing the {k + 1} feature')
-                current_set_of_features.remove(k)
-                accuracy = dummy_function(data, current_set_of_features, k)
-                print(f'Using feature(s) {current_set_of_features} accuracy is {accuracy * 100}%')
-                if accuracy > best_so_far_accuracy:
-                    best_so_far_accuracy = accuracy
-                    feature_to_remove_at_this_level = k
-                else:
-                    current_set_of_features.append(k)
-        if feature_to_remove_at_this_level is not None:
-            print(f'Feature set {current_set_of_features} was best, accuracy is {best_so_far_accuracy * 100}%')
-
-    print(f'\nFinished search!! The best feature subset is {current_set_of_features}, which has an accuracy of {best_so_far_accuracy * 100}%')
+    print(f'\nFinished search!! The best feature subset is {res}, which has an accuracy of {best_so_far_accuracy * 100}%')
 
 # Take number of features as input from user
 num_features = int(input("Enter the number of features: "))
