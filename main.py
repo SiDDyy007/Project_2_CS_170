5400e1ca0032c086fbb56d5d52a29440c89ed604
SiDDyy007 <siddhantp0@gmail.com>
2023-05-20 11:50:31 -0700

    Forward and Backward Fucntions

--- main.py
+++ main.py
@@ -13,9 +13,14 @@ def feature_search_demo(num_features):
     print("Welcome to Siddhant's Feature Selection Algorithm.")
     print(f"Please enter total number of features: {data.shape[1] - 1}")
     print("Type the number of the algorithm you want to run.")
-    print("1. Forward Selection\n2. Backward Elimination")
-    print("1")
-
+    print("1. Forward Selection\n2. Backward Elimination\n")
+    choice = int(input())
+    if choice == 1:
+        forward_selection(data, current_set_of_features)
+    else:
+        backward_elimination(data, current_set_of_features)
+    
+def forward_selection(data, current_set_of_features):
     # Using no features and “random” evaluation
     accuracy = dummy_function(data, current_set_of_features, 0)
     print(f"Using no features and “random” evaluation, I get an accuracy of {accuracy * 100}%")
@@ -38,6 +43,33 @@ def feature_search_demo(num_features):
                 else:
                     current_set_of_features.remove(k)
         if feature_to_add_at_this_level is not None:
+            print(f'Feature set {current_set_of_features} was best, accuracy is {best_so_far_accuracy * 100}%')
+
+    print(f'\nFinished search!! The best feature subset is {current_set_of_features}, which has an accuracy of {best_so_far_accuracy * 100}%')
+
+def backward_elimination(data, current_set_of_features):
+    # Using all features and “random” evaluation
+    accuracy = dummy_function(data, current_set_of_features, 0)
+    print(f"Using all features and “random” evaluation, I get an accuracy of {accuracy * 100}%")
+    print("Beginning search.")
+
+    # Iteratively eliminate features
+    for i in range(data.shape[1] - 1):
+        print(f'\n On the {i + 1}th level of the search tree')
+        feature_to_remove_at_this_level = None
+        best_so_far_accuracy = 0
+        for k in range(data.shape[1] - 1):
+            if k in current_set_of_features:
+                print(f'Considering removing the {k + 1} feature')
+                current_set_of_features.remove(k)
+                accuracy = dummy_function(data, current_set_of_features, k)
+                print(f'Using feature(s) {current_set_of_features} accuracy is {accuracy * 100}%')
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_remove_at_this_level = k
+                else:
+                    current_set_of_features.append(k)
+        if feature_to_remove_at_this_level is not None:
             print(f'Feature set {current_set_of_features} was best, accuracy is {best_so_far_accuracy * 100}%')
 
     print(f'\nFinished search!! The best feature subset is {current_set_of_features}, which has an accuracy of {best_so_far_accuracy * 100}%')
