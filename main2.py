772b3b32331c1bcf0ebfa0910bb64aa953feeadc
SiDDyy007 <siddhantp0@gmail.com>
2023-05-28 09:25:55 -0700

    Commenting work

--- main2.py
+++ main2.py
@@ -1,106 +1,143 @@
-import time
 import numpy as np
+import time
 
-def train_classifier(data, current_set_features):
-    start_time = time.time()
 
-    number_correctly_classified = 0
-    for i in range(data.shape[0]):
-        object_to_classify = data[i, current_set_features]
-        label_object_to_classify = data[i, 0]
+def euclidean_distance(a, b):
+    return sum((e1-e2)**2 for e1, e2 in zip(a,b))**0.5
 
+def normalize_data(data):
+        """
+        This function normalizes the data to have zero mean and unit variance.
+        """
+        mean = np.mean(data[:, 1:], axis=0)
+        std = np.std(data[:, 1:], axis=0)
+        std[std == 0] = 1
+        data[:, 1:] = (data[:, 1:] - mean) / std
+        return data
+
+class NN_Classifier:
+    def __init__(self, data, feature_subset=None):
+        # Normalize data at initialization
+        self.data = normalize_data(data)
+        # If no subset of features is specified, use all features
+        if feature_subset is None:
+            self.feature_subset = list(range(1, self.data.shape[1]))  
+        else:
+            self.feature_subset = feature_subset    
+
+    def train(self):
+        # Start recording time for training
+        start_time = time.time()
+        number_correctly_classified = 0
+        for i in range(self.data.shape[0]):
+            # Select instance for classification
+            object_to_classify = self.data[i, self.feature_subset]
+            label_object_to_classify = self.data[i, 0]
+
+            nearest_neighbor_distance = np.inf
+            nearest_neighbor_location = np.inf
+            # Loop through all data points
+            for k in range(self.data.shape[0]):
+                # Ensure the data point is not the one being classified
+                if k != i:
+                    # Compute the Euclidean distance between the data points
+                    distance = euclidean_distance(object_to_classify, self.data[k, self.feature_subset])
+                    # Update nearest neighbor if this data point is closer
+                    if distance < nearest_neighbor_distance:
+                        nearest_neighbor_distance = distance
+                        nearest_neighbor_location = k
+            # If the nearest neighbor has the same label, increment the count of correctly classified instances
+            nearest_neighbor_label = self.data[nearest_neighbor_location, 0]
+            if label_object_to_classify == nearest_neighbor_label:
+                number_correctly_classified += 1
+        # Compute the accuracy
+        accuracy = number_correctly_classified / self.data.shape[0]
+
+        # End recording time for training and print elapsed time
+        end_time = time.time()
+        elapsed_time = round(end_time - start_time, 4)
+        print(f'Time taken to train the classifier: {elapsed_time*10} milliseconds')
+        
+        return accuracy
+    
+    def test(self, instance):
+        # Start recording time for testing
+        start_time = time.time()
+        object_to_classify = self.data[instance, self.feature_subset]
         nearest_neighbor_distance = np.inf
         nearest_neighbor_location = np.inf
-        for k in range(data.shape[0]):
-            if k != i:
-                distance = np.sqrt(np.sum((object_to_classify - data[k, current_set_features]) ** 2))
+        # Loop through all data points
+        for k in range(self.data.shape[0]):
+            # Ensure the data point is not the one being classified
+            if k != instance:
+                # Compute the Euclidean distance between the data points
+                distance = euclidean_distance(object_to_classify, self.data[k, self.feature_subset])
+                # Update nearest neighbor if this data point is closer
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = k
-        nearest_neighbor_label = data[nearest_neighbor_location, 0]
-        if label_object_to_classify == nearest_neighbor_label:
-            number_correctly_classified += 1
-    accuracy = number_correctly_classified / data.shape[0]
-
-    end_time = time.time()
-    elapsed_time = round(end_time - start_time, 4)
-
-    print(f'Time taken to train the classifier: {elapsed_time*100} milliseconds')
-    return accuracy
+        # End recording time for testing and print elapsed time
+        end_time = time.time()
+        elapsed_time = round(end_time - start_time, 4)
+        print(f'Time taken to test the classifier: {elapsed_time*10} milliseconds')
+        
+        # Return the label of the nearest neighbor
+        return self.data[nearest_neighbor_location, 0]
 
 
-def test_classifier(data, instance, current_set_features):
-    start_time = time.time()
-    object_to_classify = data[instance, current_set_features]
-    nearest_neighbor_distance = np.inf
-    nearest_neighbor_location = np.inf
-    for k in range(data.shape[0]):
-        if k != instance:
-            distance = np.sqrt(np.sum((object_to_classify - data[k, current_set_features]) ** 2))
-            if distance < nearest_neighbor_distance:
-                nearest_neighbor_distance = distance
-                nearest_neighbor_location = k
-    end_time = time.time()
-    elapsed_time = round(end_time - start_time, 4)
+class Validator:
+    def __init__(self, data, feature_subset=None):
+        self.data = data
+        # If no subset of features is specified, use all features
+        if feature_subset is None:
+            self.feature_subset = list(range(1, self.data.shape[1]))  
+        else:
+            self.feature_subset = feature_subset
+        # Initialize the nearest neighbors classifier with the selected features
+        self.nn_classifier = NN_Classifier(self.data, self.feature_subset)
+    
+    def validate(self):
+        # Train the classifier and get the accuracy
+        accuracy = self.nn_classifier.train()
+        # Return the accuracy of the classifier
+        return accuracy
 
-    print(f'Time taken to test the classifier: {elapsed_time*100} milliseconds')
-    return data[nearest_neighbor_location, 0]
 
-def normalize_data(data):
-    """
-    This function normalizes the data to have zero mean and unit variance.
-    """
-    # We only normalize the features (not the labels), so we use data[:, 1:] 
-    mean = np.mean(data[:, 1:], axis=0)
-    std = np.std(data[:, 1:], axis=0)
 
-    # Avoid division by zero
-    std[std == 0] = 1
+def start():
+        choice = input("What would you like to do?\n1. Test the classifier using a given subset of features (Validator Class)\n2. Predict the class label for a given instance (Classifier Class)\nEnter choice (1 or 2): ")
+        if choice == '1':
+            feature_subset = feature_input()
+            data = get_dataset_input()
+            classifier = Validator(data, feature_subset)
+            accuracy = classifier.validate()
+            print(f'\nThe accuracy of the classifier using the given subset of features is: {accuracy * 100}%')
+        elif choice == '2':
+            data = get_dataset_input()
+            instance = int(input("Please specify the instance ID (Starting from 1) you would like to classify: "))            
+            classifier = NN_Classifier(data)
+            predicted_label = classifier.test(instance-1)
+            print(f'\nThe predicted label for the {instance}th instance ID is: {predicted_label}')
+        else:
+            print("Invalid choice. Please enter 1 to test the classifier or 2 to predict an instance.")
 
-    # Normalize features
-    data[:, 1:] = (data[:, 1:] - mean) / std
-
-    return data
-
-def get_input():
+def get_dataset_input():
     print ("Which dataset would you like to use?\n1. Small dataset\n2. Large dataset\nEnter choice (1 or 2): ")
     dataset_choice = input()
     if dataset_choice == '1':
         data = np.loadtxt(r'small-test-dataset.txt')
-        
     elif dataset_choice == '2':
         data = np.loadtxt(r'large-test-dataset-1.txt')
     else:
         print("Invalid choice. Please enter 1 for small dataset or 2 for large dataset.")
         exit(1)
-    # Normalizing before training or testing
-    data = normalize_data(data)
-    feature_input(data)
-    
-def feature_input(data):
-    choice = input("Would you like to specify a subset of features?\n1. Yes\n2. No\n-> ")
-    if choice == '1':
-        current_set_features = input("Please enter the indices of the features you would like to train on, separated by commas: ")
-        current_set_features = [int(x) for x in current_set_features.split(',')]
-    else:
-        current_set_features = list(range(1, data.shape[1]))  # Use all features
+    return data
 
-    accuracy = train_classifier(data, current_set_features)
-    print(f'\nThe accuracy of the classifier using {"all" if choice != "1" else "the given subset of"} features is: {accuracy * 100}%')
-    addFeature = input("Would you like to add a new feature to the classifer?\n1. Yes\n2. No\n->")
-    if addFeature == '1':
-        newFeature = int(input("Enter the feature to be added\n->"))
-        current_set_features += [newFeature]
-        accuracy = train_classifier(data, current_set_features)
-        print(f'\nThe accuracy of the classifier after adding feature - {newFeature} is: {accuracy * 100}%')
+def feature_input():
+    current_set_features = input("Please enter the features you would like to train on, separated by commas: ")
+    current_set_features = [int(x) for x in current_set_features.split(',')]
+    return current_set_features
 
-    classify = input("Do you want to test the classifier using an instance point?\n1. Yes\n2. No\n-> ")
-    if classify == '1':
-        instance = int(input("Please specify the instance index you would like to classify: "))
-        predicted_label = test_classifier(data, instance, current_set_features)
-        print(f'\nThe predicted label for the {instance+1}th object is: {predicted_label}')
-    else:
-        print("Thank you! Program Done!")
+start()
 
 
-get_input()
